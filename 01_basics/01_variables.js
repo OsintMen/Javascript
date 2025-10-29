17ef26d9ca7084b1d355a3fc3d9f929fc8796b4f
non-e <osintmind101@gmail.com>
2025-10-29 10:34:58 +0000

    configure container done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1345787;
+let accountMail = "osint@test.com";
+var accountPass = "340745";
+acountCity = "NYC";
+accountState; 
+
+//accountId= 4379854 //not allowed
+
+accountMail="os@test.com";
+accountPass="74584390";
+acountCity ="Houston";
+
+
+console.log("acountId");
+
+//print in the form of table structure
+console.table([accountId ,accountMail ,accountPass, acountCity ,accountState]);
